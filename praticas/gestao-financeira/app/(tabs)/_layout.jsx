1c350dddd205b4de281ed6e36cdcfe288d0efebc
Ironildo Junior <ironildojr.1@gmail.com>
2026-04-08 21:40:59 -0300

    feature: iniciando projeto de gestao financeira

---
+++ praticas/gestao-financeira/app/(tabs)/_layout.jsx
@@ -0,0 +1,77 @@
+import { Tabs } from "expo-router";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "../../constants/colors";
+
+export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: colors.primary },
+        headerTintColor: colors.primaryContrast,
+        headerTitleAlign: "center",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.inactive,
+        tabBarStyle: {
+          height: 60,
+          paddingTop: 5,
+          bottom: insets.bottom,
+          backgroundColor: colors.background,
+        },
+        tabBarButton: (props) => (
+          <TouchableOpacity {...props} activeOpacity={0.8} />
+        ),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Transações",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="attach-money" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add-transactions"
+        options={{
+          title: "Adicionar Transação",
+          tabBarLabel: "",
+          tabBarIcon: () => (
+            <View style={styles.addButton}>
+              <MaterialIcons
+                name="add"
+                size={40}
+                color={colors.primaryContrast}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="summary"
+        options={{
+          title: "Resumo",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="pie-chart" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+const styles = StyleSheet.create({
+  addButton: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 64,
+    width: 64,
+    borderRadius: 32,
+    backgroundColor: colors.primary,
+  },
+});
---
+++ praticas/gestao-financeira/app/_layout.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { colors } from "../constants/colors";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar backgroundColor={colors.primary} style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
+  );
+}

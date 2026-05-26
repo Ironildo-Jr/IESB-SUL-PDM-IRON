637e2397747186e18711ede8f984c329bad9bcbf
Ironildo Junior <ironildojr.1@gmail.com>
2026-05-25 23:00:10 -0300

    feat: add picker para as categorias e datepicker para a data

--- praticas/gestao-financeira/app/(tabs)/add-transactions.jsx
+++ praticas/gestao-financeira/app/(tabs)/add-transactions.jsx
@@ -1,23 +1,32 @@
+import { useState } from "react";
 import {
-  Text,
-  View,
-  ScrollView,
-  TextInput,
   Alert,
-  StyleSheet,
   Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import { globalStyles } from "../../styles/globalStyles";
-import { useState } from "react";
+
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
+import { categories } from "../../constants/categories";
+import { colors } from "../../constants/colors";
+
 export default function AddTransactions() {
   const initialForm = {
     description: "",
     value: 0,
-    date: "",
-    category: "Renda",
+    date: new Date(),
+    category: "income",
   };
-   // Estado inicial como um objeto
+  // Estado inicial como um objeto
   const [form, setForm] = useState(initialForm);
+  const [showPicker, setShowPicker] = useState(false);
 
   const addTransaction = () => {
     Alert.alert(
@@ -25,10 +34,23 @@ export default function AddTransactions() {
     );
   };
 
+  const handleCurrencyChange = (text) => {
+    const formattedValue = text.replace(/\D/g, ""); // Remove caracteres não numéricos
+    const numericValue = parseFloat(formattedValue) / 100; // Converte para número decimal com centavos
+    setForm({ ...form, value: numericValue }); // Atualiza o estado com o valor numérico
+  };
+
+  const handleDateChange = (_, selectedDate) => {
+    setShowPicker(false); // Esconde o DatePicker após a seleção
+    if (selectedDate) {
+      setForm({ ...form, date: selectedDate }); // Atualiza o estado com a data formatada
+    }
+  };
+
   return (
     <View style={globalStyles.screenContainer}>
       <ScrollView style={globalStyles.content}>
-        <View style={StyleSheet.form}>
+        <View style={styles.form}>
           <View>
             <Text style={globalStyles.inputLabel}>Descrição</Text>
             <TextInput
@@ -41,29 +63,51 @@ export default function AddTransactions() {
           <View>
             <Text style={globalStyles.inputLabel}>Valor</Text>
             <TextInput
-              value={form.value}
-              onChangeText={(text) => setForm({ ...form, value: text })}
+              value={form.value.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+              onChangeText={(text) => handleCurrencyChange(text)}
               keyboardType="numeric"
               style={globalStyles.input}
             />
           </View>
-
           <View>
             <Text style={globalStyles.inputLabel}>Data</Text>
-            <TextInput
-              value={form.date}
-              onChangeText={(text) => setForm({ ...form, date: text })}
-              style={globalStyles.input}
-            />
+            <TouchableOpacity onPress={() => setShowPicker(true)}>
+              <TextInput
+                value={form.date.toLocaleDateString("pt-BR")}
+                onChangeText={(text) => setForm({ ...form, date: text })} // Permite digitar a data manualmente
+                style={globalStyles.input}
+                editable={showPicker} // Permite edição apenas quando o DatePicker estiver visível
+              />
+            </TouchableOpacity>
+            {showPicker && (
+              <RNDateTimePicker
+                value={form.date}
+                display={Platform.OS === "ios" ? "inline" : "default"}
+                mode="date"
+                onChange={handleDateChange}
+              />
+            )}
           </View>
 
           <View>
             <Text style={globalStyles.inputLabel}>Categoria</Text>
-            <TextInput
-              value={form.category}
-              onChangeText={(text) => setForm({ ...form, category: text })}
-              style={globalStyles.input}
-            />
+            <View style={styles.picker}>
+              <Picker
+                selectedValue={form.category}
+                onValueChange={(text) => setForm({ ...form, category: text })}
+              >
+                {categories.map((item, index) => (
+                  <Picker.Item
+                    key={index}
+                    label={item.displayName}
+                    value={item.name}
+                  />
+                ))}
+              </Picker>
+            </View>
           </View>
         </View>
 
@@ -72,3 +116,20 @@ export default function AddTransactions() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    gap: 12,
+    marginBottom: 40,
+    marginTop: 10,
+  },
+  picker: {
+    display: "flex",
+    justifyContent: "center",
+    height: 44,
+    borderColor: colors.secondaryText,
+    borderWidth: 1,
+    borderRadius: 8,
+    flexGrow: 1,
+  },
+});
---
+++ praticas/gestao-financeira/constants/categories.js
@@ -0,0 +1,34 @@
+import { colors } from "./colors";
+
+export const categories = [
+  {
+    icon: "work",
+    background: colors.categoryIncome,
+    name: "income",
+    displayName: "Renda",
+  },
+  {
+    icon: "fastfood",
+    background: colors.categoryFood,
+    name: "food",
+    displayName: "Alimentação",
+  },
+  {
+    icon: "home",
+    background: colors.categoryHouse,
+    name: "house",
+    displayName: "Casa",
+  },
+  {
+    icon: "book",
+    background: colors.categoryEducation,
+    name: "education",
+    displayName: "Educação",
+  },
+  {
+    icon: "airplanemode-active",
+    background: colors.categoryTravel,
+    name: "travel",
+    displayName: "Viagens",
+  },
+];
